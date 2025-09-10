f690d39355df7e063ff67e8a95889f4ca435ecfd
Anton Tkachuk <tkacuk2291@gmail.com>
2025-09-10 17:10:05 +0300

    test deploy

--- frontEnd-e-commerce/services/api.js
+++ frontEnd-e-commerce/services/api.js
@@ -1,10 +1,11 @@
 export async function  get(url){
-    const baseUrl = 'http://127.0.0.1:8000/'
+    const baseUrl = import.meta.env.VITE_API_URL
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-    const myRequest = new Request(baseUrl + url , {
+    const myRequest = new Request("https://e-commerce-feature.onrender.com/" + url , {
         method: "GET",
         headers: myHeaders,
     });
     return  await  fetch(myRequest)
 }
+
